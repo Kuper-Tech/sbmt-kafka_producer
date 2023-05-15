dafd95839816af303aa3ac934b90e6a510134bb6
denis.tarasenko <denis.tarasenko@sbermarket.ru>
2023-05-16 02:36:22 +0300

    [DEX-1088] feat: add kafka options

--- lib/sbmt/kafka_producer/configs/kafka.rb
+++ lib/sbmt/kafka_producer/configs/kafka.rb
@@ -15,12 +15,22 @@ module Sbmt
         on_load :ensure_options_are_valid
 
         def to_kafka_options
-          kafka_config.merge(
+          custom_kafka_config.merge(
             "bootstrap.servers": servers
           ).symbolize_keys
         end
 
         private
+
+        def custom_kafka_config
+          {
+            "socket.connection.setup.timeout.ms": kafka_config.dig("connect_timeout").to_f * 1000,
+            "request.timeout.ms": kafka_config.dig("ack_timeout").to_f * 1000,
+            "request.required.acks": kafka_config.dig("required_acks"),
+            "message.send.max.retries": kafka_config.dig("max_retries"),
+            "retry.backoff.ms": kafka_config.dig("retry_backoff").to_f * 1000
+          }
+        end
 
         def ensure_options_are_valid
           raise_validation_error "invalid servers: #{servers}, should be in format: \"host1:port1,host2:port2,...\"" unless SERVERS_REGEXP.match?(servers)
