d8284f0b0917f5e8c2a04b3ea1e54128747e36b9
denis.tarasenko <denis.tarasenko@sbermarket.ru>
2023-05-19 09:29:21 +0300

    [DEX-1088] feat: add default value in section kafka

--- lib/sbmt/kafka_producer/configs/kafka.rb
+++ lib/sbmt/kafka_producer/configs/kafka.rb
@@ -7,8 +7,12 @@ module Sbmt
         SERVERS_REGEXP = /^[\w\d.\-:,]+$/.freeze
 
         config_name :kafka_producer_kafka
-        attr_config :servers, :connect_timeout, :ack_timeout,
-          :required_acks, :max_retries, :retry_backoff,
+        attr_config :servers,
+          connect_timeout: 1,
+          ack_timeout: 1,
+          required_acks: 1,
+          max_retries: 2,
+          retry_backoff: 1,
           kafka_config: {}
 
         required :servers
@@ -31,11 +35,6 @@ module Sbmt
 
         def ensure_options_are_valid
           raise_validation_error "invalid servers: #{servers}, should be in format: \"host1:port1,host2:port2,...\"" unless SERVERS_REGEXP.match?(servers)
-          raise_validation_error "invalid connect_timeout: cannot be empty" unless connect_timeout&.is_a?(Integer)
-          raise_validation_error "invalid ack_timeout: cannot be empty" unless ack_timeout&.is_a?(Integer)
-          raise_validation_error "invalid required_acks: cannot be empty" unless required_acks&.is_a?(Integer)
-          raise_validation_error "invalid max_retries: cannot be empty" unless max_retries&.is_a?(Integer)
-          raise_validation_error "invalid retry_backoff: cannot be empty" unless retry_backoff&.is_a?(Integer)
         end
       end
     end
