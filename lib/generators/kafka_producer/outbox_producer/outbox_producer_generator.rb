e877539a57e8195ec2ed32e6a4919182cb094ad4
denis.tarasenko <denis.tarasenko@sbermarket.ru>
2023-07-22 10:00:41 +0300

    [DEX-1242] refactor: fix comments

--- lib/generators/kafka_producer/outbox_producer/outbox_producer_generator.rb
+++ lib/generators/kafka_producer/outbox_producer/outbox_producer_generator.rb
@@ -1,14 +1,46 @@
 # frozen_string_literal: true
 
-require "generators/base_producer_generator"
+require "rails/generators/named_base"
+require "psych"
 
 module KafkaProducer
   module Generators
-    class OutboxProducerGenerator < BaseProducerGenerator
+    class OutboxProducerGenerator < Rails::Generators::NamedBase
       source_root File.expand_path("templates", __dir__)
 
-      def kind
-        :outbox_producer
+      OUTBOX_CONFIG_PATH = "config/outbox.yml"
+
+      def insert_outbox_producer
+        return section_missing unless outbox_item_exists?
+
+        pattern = /#{item_name}.*?(exponential_backoff)\s*\n/m
+        inject_into_file OUTBOX_CONFIG_PATH, optimize_indentation(template, 6), after: pattern, force: true
+      end
+
+      private
+
+      def load_yaml
+        @load_yaml ||= Psych.load_file(OUTBOX_CONFIG_PATH)
+      end
+
+      def item_name
+        "#{name}_outbox_item"
+      end
+
+      def section_missing
+        $stdout.puts "Could not find section #{item_name} in config/outbox.yml"
+      end
+
+      def outbox_item_exists?
+        load_yaml.dig("default", "outbox_items", item_name).present?
+      end
+
+      def template
+        <<~YAML
+          transports:
+            sbmt/kafka_producer:
+              topic: 'need to add a topic'
+        YAML
       end
     end
   end
