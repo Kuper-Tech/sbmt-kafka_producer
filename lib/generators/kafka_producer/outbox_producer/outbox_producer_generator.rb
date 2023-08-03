1f21d7ed756f30ad861f79a258f61c95e79b957c
kalinkin.ve <vadim.kalinkin@sbermarket.ru>
2023-08-03 16:59:17 +0500

    [DEX-1242] refactor: templates
    [DEX-1242](https://jira.sbmt.io/browse/DEX-1242)

--- lib/generators/kafka_producer/outbox_producer/outbox_producer_generator.rb
+++ lib/generators/kafka_producer/outbox_producer/outbox_producer_generator.rb
@@ -7,7 +7,10 @@ module KafkaProducer
     class OutboxProducerGenerator < Rails::Generators::NamedBase
       source_root File.expand_path("templates", __dir__)
 
+      class_option :skip_item, type: :boolean, default: false, desc: "Skip creating InboxItem"
+
       def insert_outbox_producer
+        generate "outbox:item", "#{item_name.underscore} --kind outbox" unless options[:skip_item]
         generate "outbox:transport", "#{item_name.underscore} sbmt/kafka_producer --kind outbox"
       end
 
